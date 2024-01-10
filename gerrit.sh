d1d8a7e493179008b364a551dab9b605d1caf9f5
i314q159 <i314q159@outlook.com>
2024-01-10 17:04:47 +0800

    update

---
+++ gerrit.sh
@@ -0,0 +1,1 @@
+ssh -p 29418 hewenbo@192.168.10.100 gerrit show-caches
--- main.py
+++ main.py
@@ -17,34 +17,34 @@ def get_name(xml_path):
     return projects
 
 
-def create_project(name):
-    cmd = f"gerrit create-project --empty-commit --branch master {parent_repo}/{name}"
+def create_project(name, parent_name):
+    cmd = f"gerrit create-project --empty-commit --branch master --parent {parent_name} {parent_name}/{name}"
     stdin, stdout, stderr = ssh.exec_command(cmd)
 
     if stdout.channel.recv_exit_status() != 0:
-        print(f"failed to create project: {parent_repo}/{name}")
+        print(f"failed to create project: {PARENT_NAME}/{name}")
     print(cmd)
 
 
 def set_project_parent(name, parent_name):
-    cmd = f"gerrit set-project-parent --parent {parent_name} {parent_repo}/{name}"
+    cmd = f"gerrit set-project-parent --parent {parent_name} {parent_name}/{name}"
 
     stdin, stdout, stderr = ssh.exec_command(cmd)
 
     if stdout.channel.recv_exit_status() != 0:
-        print(f"failed to set project: {parent_repo}/{name}")
+        print(f"failed to set project: {PARENT_NAME}/{name}")
     print(cmd)
 
 
 if __name__ == "__main__":
-    parent_repo = input("parent repo name: ") or f"i314q159"
-    xml_path = input("manifest xml: ") or f"./default.xml"
+    PARENT_NAME = input("parent repo name: ") or f"i314q159"
+    XML_PATH = input("manifest xml: ") or f"./default.xml"
 
-    projects = get_name(xml_path=xml_path).copy()
+    projects = get_name(xml_path=XML_PATH).copy()
 
-    GERRIT_URL = input("gerrit ip: ") or f"127.0.0.1"
+    GERRIT_URL = input("gerrit ip: ") or f"192.168.10.100"
     GERRIT_PORT = input("gerrit port: ") or 29418
-    USERNAME = input("gerrit user name: ") or f"admin"
+    USERNAME = input("gerrit user name: ") or f"hewenbo"
 
     print(f"{USERNAME}@{GERRIT_URL}:{GERRIT_PORT}")
 
@@ -54,7 +54,8 @@ if __name__ == "__main__":
 
     try:
         for project in projects:
-            # create_project(project["name"])
-            set_project_parent(project["name"], parent_name=parent_repo)
+            create_project(project["name"], parent_name=PARENT_NAME)
+            # set_project_parent(project["name"], parent_name=PARENT_NAME)
+            pass
     finally:
         ssh.close()
